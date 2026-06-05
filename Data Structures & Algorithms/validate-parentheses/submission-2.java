5045ecc5b40d182d0f3db93d20429398b0a6b83c
Yurim Jang <101785865+urimJ@users.noreply.github.com>
2026-06-05 09:58:34 +0900

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '[' || c == '{' || c == '('){
+                stack.push(c);
+            }
+            else{
+                if(stack.isEmpty()) return false;
+                else {
+                    if(c==']'){
+                        if(stack.peek()!='[') return false;
+                    }
+                    if(c=='}'){
+                        if(stack.peek()!='{') return false;
+                    }
+                    if(c==')'){
+                        if(stack.peek()!='(') return false;
+                    }
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty(); // 스택이 비어 있어야 함
+    }
+}
